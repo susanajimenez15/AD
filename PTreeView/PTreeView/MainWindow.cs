2abfb9e14fb3f4a3e6070fed95188245008b841e
susanajimenez15 <dam.sjimenez@institutoserpis.org>
2016-10-05 08:26:42 +0200

    Añadimos biblioteca propia y creamos metodo TreeViewHelper

--- PTreeView/PTreeView/MainWindow.cs
+++ PTreeView/PTreeView/MainWindow.cs
@@ -2,30 +2,45 @@ using System;
 using Gtk;
 using System.Data;
 using MySql.Data.MySqlClient;
+using Org.InstitutoSerpis.Ad;
+
 
 public partial class MainWindow: Gtk.Window
 {	
 	public MainWindow (): base (Gtk.WindowType.Toplevel)
 	{
 		Build ();
-		treeview.AppendColumn ("id", new CellRendererText(), "text", 0);
-		treeview.AppendColumn ("nombre", new CellRendererText (), "text", 1);
-		treeview.AppendColumn ("precio", new CellRendererText (), "text", 2);
-			treeview.AppendColumn("categoria", new CellRendererText (), "text", 3);
 
+		string[] columnNames = {"id", "nombre", "precio"};
+		TreeViewHelper.AppendColumns(treeView,columnNames);
+		//treeview.AppendColumn ("id", new CellRendererText(), "text", 0);
+		//treeview.AppendColumn ("nombre", new CellRendererText (), "text", 1);
+		/*treeview.AppendColumn ("precio", new CellRendererText (),
+		                       delegate(TreeViewColumn tree_column, CellRenderer CellView, TreeModel tree_model,
+		         CellRendererText CellRendererText=(CellRendererText)cell)
+		 );
+
+
+		//los delegados no se ejecutan hasta que alguien lo llame 
+		treeview.AppendColumn("categoria", new CellRendererText (), "text", 3);
+
+		//IMP: el typeof(decimal) no es reconocido por el treeview
 		ListStore listStore = new ListStore (typeof(long), typeof(string), typeof(string), typeof(long));
 
 		treeview.Model = listStore;
+
+		listStore.AppendValues(1L, "categoria 1", 1)
 
 		IDbConnection connect = new MySqlConnection ("Database=dbprueba;user=root;password=sistemas");
 		connect.Open ();
 
 		fillListStore (listStore, connect);
 
-
+	 */
 	}
 
-	protected void fillListStore (ListStore listStore, IDbConnection connect){
+	protected void fillListStore (ListStore listStore, IDbConnection connect)
+	{
 		listStore.Clear ();
 		IDbCommand dbcommand = connect.CreateCommand ();
 		dbcommand.CommandText = "select * from articulo order by id";
@@ -40,5 +55,10 @@ public partial class MainWindow: Gtk.Window
 	{
 		Application.Quit ();
 		a.RetVal = true;
+	}
+
+	protected void listar()
+	{
+	
 	}
 }
