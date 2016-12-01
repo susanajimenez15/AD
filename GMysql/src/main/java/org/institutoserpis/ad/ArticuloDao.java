f5963dca5c2317d0b80760a67e55155d57088d73
susanajimenez15 <dam.sjimenez@institutoserpis.org>
2016-12-01 09:55:13 +0100

    JDBC - Proyecto BD con Java-Eclipse

--- GMysql/src/main/java/org/institutoserpis/ad/ArticuloDao.java
+++ GMysql/src/main/java/org/institutoserpis/ad/ArticuloDao.java
@@ -1,5 +1,31 @@
 package org.institutoserpis.ad;
 
-public class ArticuloDao {
+import java.sql.*;
 
+public class ArticuloDao {
+	
+	public static void main(String [] args) throws SQLException{
+
+		//Si no indicamos la base de datos, se conectara al servidor solamente
+		Connection connection = DriverManager.getConnection("jdbc:mysql://localhost/dbprueba", "root", "sistemas");
+		
+		Statement statement = connection.createStatement();
+		ResultSet resultSet = statement.executeQuery("select * from articulo");
+		
+		//Para sacarlo con fromato
+		//Sustituye %5d\ por el primer valor de la variable
+		System.out.printf("%5s %30s %10s  %5s\n", "Id", "Nombre", "Precio", "Categoria");
+		System.out.println();
+		while(resultSet.next()) {
+			System.out.printf("%5d  %30s %10s  %5s\n", resultSet.getObject("id"), resultSet.getObject("nombre"), resultSet.getObject("precio"), resultSet.getObject("categoria") );
+			//System.out.printf(" nombre = %s\n", resultSet.getObject("nombre"));
+		}
+		
+		statement.close();
+		connection.close();
+		System.out.println("FIN DE LA CONEXION");
+		
+		
+		
+	}
 }
