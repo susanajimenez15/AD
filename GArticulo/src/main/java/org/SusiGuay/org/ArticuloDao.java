4c1a66dd1420e2410a8356419b5dfcbd8f062cfb
susanajimenez15 <dam.sjimenez@institutoserpis.org>
2016-12-13 11:27:51 +0100

    Nuevo GArticulo - Java

--- GArticulo/src/main/java/org/SusiGuay/org/ArticuloDao.java
+++ GArticulo/src/main/java/org/SusiGuay/org/ArticuloDao.java
@@ -1,10 +1,12 @@
-package org.institutoserpis.ad;
+package org.SusiGuay.org;
 
 import java.awt.Menu;
 import java.sql.*;
 import java.util.Scanner;
 
 public class ArticuloDao {
+	
+	static Scanner scanner = new Scanner(System.in);
 	
 	public static void main(String [] args) throws SQLException{
 
@@ -35,7 +37,38 @@ public class ArticuloDao {
 			);
 			//System.out.printf(" nombre = %s\n", resultSet.getObject("nombre"));
 		}
+		int opc = 0;
 		
+		do{
+			System.out.println("------------------ MENU ------------------");
+			System.out.println("1. Nuevo articulo. ");
+			System.out.println("2. Modificar articulo. ");
+			System.out.println("3. Eliminar articulo. ");
+			System.out.println("4. Consultar articulo.");
+			System.out.println("5. Listar todos los articulos. ");
+			System.out.println("0. Salir");
+			System.out.println("------------------------------------------");
+			System.out.println("Introduce un valor: ");
+			opc = scanner.nextInt();
+		
+			switch (opc) {
+				case 1:
+					nuevo();
+					
+				case 2:
+					
+				case 3:
+					
+				case 4:
+					
+				case 5:
+					
+				case 0:
+					System.out.println("Adios");
+					
+				
+			}
+		}while( opc == 0 );
 		
 		preparedStatement.close();
 		connection.close();
@@ -45,5 +78,22 @@ public class ArticuloDao {
 		
 	}
 
+
+	public static void nuevo() throws SQLException{
+		
+		
+		System.out.println("Nuevo articulo: ");
+		System.out.println("Nombre del articulo: ");
+		String nombre = scanner.nextLine();
+		System.out.println("Precio del articulo: ");
+		double precio = scanner.nextDouble();
+		System.out.println("Categoria del articulo (numero) : ");
+		int categoria = scanner.nextInt();
+		
+
+		
+		
+		
+	}
 
 }
